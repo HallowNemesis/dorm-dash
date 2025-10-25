4eb6e69e2d3313cad2b7ff6c3cb6dafc7330e2a1
Ivy@RecompiledBirds <52045489+RecompiledBirds@users.noreply.github.com>
2025-10-25 13:51:55 -0400

    Create basic login screen on react native app

--- dormdash-frontend/app/index.tsx
+++ dormdash-frontend/app/index.tsx
@@ -1,15 +1,42 @@
-import { Text, View } from "react-native";
-
+import { Button } from "@react-navigation/elements";
+import { useState } from "react";
+import { Text, TextInput, View,StyleSheet, Alert } from "react-native";
+import bcrypt from 'bcrypt-react-native'
+import { has } from "lodash";
+const loginStyle = StyleSheet.create({
+   input:{
+    backgroundColor:"#bfbfbf",
+    width:"80%",
+    height:50,
+    margin:10,
+    borderRadius:10,
+   },
+   title:{
+    fontSize:50,
+    fontWeight:100
+   }
+})
 export default function Index() {
+  const [email,setEmail]= useState('')
+  const[passHash,setPassHash]=useState('')
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        marginBottom:"80%"
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={loginStyle.title}>Login</Text>
+      <TextInput style={loginStyle.input} placeholderTextColor="#6e6e6e" onChangeText={newEmail=>setEmail(newEmail)} placeholder="email@university.edu"/>
+        
+      <TextInput style={loginStyle.input} onChangeText={
+        //Eventually this should use some sort of hashing function, but i can't find a modern solution for react native (short of building our own..)
+        // I tried bcrypt and similar libraries, but they are unavailbe for react native.
+        newPass=>setPassHash(newPass)
+      } secureTextEntry={true}/>
+      <Button onPress={()=>Alert.alert(email,passHash)}>Login</Button>
     </View>
   );
 }
